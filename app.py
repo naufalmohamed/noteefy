e90df2262c4f390b96109967086aab63ba4cd18a
Naufal <naufal@ubuntu>
2021-07-23 22:22:43 +0530

    changes app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 import psycopg2 
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 
+result = urlparse("postgres://qmnyuigfxkvneg:a46f13b306a3a3de3d4e7f373f65a258203661a682b624f49c0e21a79ca863f2@ec2-54-155-254-112.eu-west-1.compute.amazonaws.com:5432/dvv1uhocoo0e9")
 
-def todo_create_table():
-	dbconn = psycopg2.connect("dbname=todo")
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+port = result.port
+
+def todo_create_table(username,password,database,hostname,port):
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("CREATE TABLE IF NOT EXISTS todo_table( id serial PRIMARY KEY, title VARCHAR(50) NOT NULL,status BOOLEAN NOT NULL);")
 	dbconn.commit()
 		
 	
-def select_from_table():
-	dbconn = psycopg2.connect("dbname=todo")
+def select_from_table(username,password,database,hostname,port):
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("SELECT * FROM todo_table;")
 	todo_list = cursor.fetchall()
@@ -22,14 +30,14 @@ def select_from_table():
 	
 @app.route("/")
 def index():
-	todo_list = select_from_table()
+	todo_list = select_from_table(username,password,database,hostname,port)
 	return render_template("base.html", todo_list = todo_list)
 	
 	
 @app.route("/add", methods=["POST"])
-def todo_add_to_table():
+def todo_add_to_table(username,password,database,hostname,port):
 	title_ret= request.form.get("title")
-	dbconn = psycopg2.connect("dbname=todo")
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("""INSERT INTO todo_table (title, status) VALUES (%s,%s);""",(title_ret, False))
 	dbconn.commit()
@@ -37,8 +45,8 @@ def todo_add_to_table():
 	
 	
 @app.route("/update/<int:todo_id>")
-def todo_update(todo_id):
-	dbconn = psycopg2.connect("dbname=todo")
+def todo_update(todo_id,username,password,database,hostname,port):
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("""UPDATE todo_table SET status = %s WHERE id = %s;""",(True,todo_id))
 	dbconn.commit()
@@ -47,7 +55,7 @@ def todo_update(todo_id):
 	
 @app.route("/update_not/<int:todo_id>")
 def todo_update_not(todo_id):
-	dbconn = psycopg2.connect("dbname=todo")
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("""UPDATE todo_table SET status = %s WHERE id = %s;""",(False,todo_id))
 	dbconn.commit()
@@ -55,8 +63,8 @@ def todo_update_not(todo_id):
 	
 	
 @app.route("/delete/<int:todo_id>")
-def todo_delete(todo_id):
-	dbconn = psycopg2.connect("dbname=todo")
+def todo_delete(todo_id,username,password,database,hostname,port):
+	dbconn = psycopg2.connect(database = database,user = username,password = password,host = hostname,port = port)
 	cursor = dbconn.cursor()
 	cursor.execute("""DELETE from todo_table WHERE id = %s;""",[todo_id])
 	dbconn.commit()
@@ -65,6 +73,5 @@ def todo_delete(todo_id):
 
 if __name__ == "__main__":
 
-	todo_create_table()
-	#todo_add_to_table()
+	todo_create_table(username,password,database,hostname,port)
 	app.run(debug=True)
